45e0f394b681714e213b8c48ea92ab19d0045ab6
shyweeds <2149934895@qq.com>
2026-04-29 20:50:29 +0800

    update:test examples,add multithread ok

--- src/wserver.c
+++ src/wserver.c
@@ -4,64 +4,97 @@
 #include <bits/pthreadtypes.h>
 #include <pthread.h>
 #include <stdio.h>
-#define MAX_TASKS 1024
+#include <time.h>
+#define MAX_BUF_TASKS 1024
 
-int buf_tasks = 1;         // set default buffer size
-int task_queue[MAX_TASKS]; // task queue
-int q_head = 0;            // head pointer of task queue
-int q_tail = 0;            // tail pointer of task queue
-int q_count = 0;           // count number of task queue
+typedef struct {
+  int buf_tasks;                 // set default buffer size
+  int task_queue[MAX_BUF_TASKS]; // task queue
+  int q_head;                    // head pointer of task queue
+  int q_tail;                    // tail pointer of task queue
+  int q_count;                   // count number of task queue
+  pthread_mutex_t q_mutex;       // task queue mutex init
+  pthread_cond_t q_empty;        // task queue cond init
+  pthread_cond_t q_full;         // task queue cond init
+} task_queue_t;
 
-pthread_mutex_t q_mutex = PTHREAD_MUTEX_INITIALIZER; // task queue mutex init
-pthread_cond_t q_cond = PTHREAD_COND_INITIALIZER;    // task queue cond init
+// need an task_queue_t(sync)
+task_queue_t q;
 
 char default_root[] = ".";
 
-/*push the ready conn_fd to buffer which will be handled by worker threads in
- * threads pool latter*/
-void task_queue_push(int conn_fd) {
-  pthread_mutex_lock(&q_mutex);
-
-  /*when the buffer is full, just wait*/
-  while (q_count == buf_tasks) {
-    pthread_cond_wait(&q_cond, &q_mutex);
-  }
-
-  task_queue[q_tail] = conn_fd;
-  q_tail = (q_tail + 1) % MAX_TASKS;
-  q_count++;
-
-  pthread_cond_signal(&q_cond);
-  pthread_mutex_unlock(&q_mutex);
+void task_queue_init(task_queue_t *q) {
+  q->buf_tasks = 1;                      // set default buffer size
+  q->q_head = 0;                         // head pointer of task queue
+  q->q_tail = 0;                         // tail pointer of task queue
+  q->q_count = 0;                        // count number of task queue
+  pthread_mutex_init(&q->q_mutex, NULL); // task queue mutex init
+  pthread_cond_init(&q->q_full, NULL);   // task queue cond init
+  pthread_cond_init(&q->q_empty, NULL);  // task queue cond init
 }
 
-int task_queue_pop(void) {
-  pthread_mutex_lock(&q_mutex);
+/*push the ready conn_fd to buffer which will be handled by worker threads in
+ * threads pool latter*/
+void task_queue_push(task_queue_t *q, int conn_fd) {
+  pthread_mutex_lock(&q->q_mutex);
+
+  /*when the buffer is full, just wait*/
+  while (q->q_count == q->buf_tasks) {
+    pthread_cond_wait(&q->q_empty, &q->q_mutex);
+  }
+
+  q->task_queue[q->q_tail] = conn_fd;
+  q->q_tail = (q->q_tail + 1) % MAX_BUF_TASKS;
+  q->q_count++;
+
+  pthread_cond_signal(&q->q_full);
+  pthread_mutex_unlock(&q->q_mutex);
+}
+
+int task_queue_pop(task_queue_t *q) {
+  pthread_mutex_lock(&q->q_mutex);
   int conn_fd = -1; // set default to an error
 
   /*when the buffer is empty, just wait*/
-  while (q_count == 0) {
-    pthread_cond_wait(&q_cond, &q_mutex);
+  while (q->q_count == 0) {
+    pthread_cond_wait(&q->q_full, &q->q_mutex);
   }
 
-  conn_fd = task_queue[q_head];
+  conn_fd = q->task_queue[q->q_head];
   assert(conn_fd != -1);
-  q_head = (q_head + 1) % MAX_TASKS;
-  q_count--;
+  q->q_head = (q->q_head + 1) % MAX_BUF_TASKS;
+  q->q_count--;
 
-  pthread_mutex_unlock(&q_mutex);
+  pthread_cond_signal(&q->q_empty);
+  pthread_mutex_unlock(&q->q_mutex);
   return conn_fd;
 }
 
+/*define the work thread*/
+void *worker_thread(void *arg) {
+  while (1) {
+    /* already handle the sync in the task_queue_pop()*/
+    /*thread will wait because of the q_cond*/
+    int conn_fd = task_queue_pop(&q);
+
+    /*handle the things*/
+    request_handle(conn_fd);
+    close_or_die(conn_fd);
+  }
+  return NULL;
+}
+
 //
-// ./wserver [-d <basedir>] [-p <portnum>]
+// prompt> ./wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s
+// schedalg]
 //
 int main(int argc, char *argv[]) {
   int c;
   char *root_dir = default_root;
   int port = 10000;
+  int thread_num = 1; // set default thread num to 1
 
-  while ((c = getopt(argc, argv, "d:p:b:")) != -1)
+  while ((c = getopt(argc, argv, "d:p:t:b:")) != -1)
     switch (c) {
     case 'd':
       root_dir = optarg;
@@ -69,16 +102,27 @@ int main(int argc, char *argv[]) {
     case 'p':
       port = atoi(optarg);
       break;
+    case 't':
+      thread_num = atoi(optarg);
+      break;
     case 'b':
-      buf_tasks = atoi(optarg);
+      q.buf_tasks = atoi(optarg);
       break;
     default:
-      fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+      fprintf(stderr, "usage: prompt> ./wserver [-d basedir] [-p port] [-t "
+                      "threads] [-b buffers] [-s schedalg]\n");
       exit(1);
     }
 
-  // run out of this directory
+  // run at this directory
   chdir_or_die(root_dir);
+
+  // create thead pool(able to handle requests)
+  task_queue_init(&q); // init the task_queue
+  pthread_t tids[thread_num];
+  for (int i = 0; i < thread_num; i++) {
+    pthread_create(&tids[i], NULL, worker_thread, NULL);
+  }
 
   // now, get to work
   int listen_fd = open_listen_fd_or_die(port);
@@ -87,7 +131,8 @@ int main(int argc, char *argv[]) {
     int client_len = sizeof(client_addr);
     int conn_fd = accept_or_die(listen_fd, (sockaddr_t *)&client_addr,
                                 (socklen_t *)&client_len);
-    task_queue_push(conn_fd);
+
+    task_queue_push(&q, conn_fd); // add conn_fd to buf_tasks(queue)
     /*Should not handle the data in master thread*/
     //    request_handle(conn_fd);
     //    close_or_die(conn_fd);
