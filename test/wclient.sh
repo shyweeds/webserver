af1601de0ddc8917c9272a189be15e2957ff9349
shyweeds <2149934895@qq.com>
2026-05-01 22:44:22 +0800

    remove tests

--- test/wclient.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../test/wclient localhost 8003 /spin.cgi & ../src/wclient localhost 8003 /a.html
--- test/wserver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../src/wserver -d ../src/basedir/ -p 8003 -t 8 -b 16 -s SFF
